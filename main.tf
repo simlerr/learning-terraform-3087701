55c7aececc54a5071e566c1f913ebcec822e08b3
simlerr <142019340+simlerr@users.noreply.github.com>
2025-01-30 14:04:29 -0600

    Create security group run 3

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ vpc_security_group_ids = [aws_security_group.blog.id]
   }
 }
 
-resource "aws"security_group" "blog" {
+resource "aws_security_group" "blog" {
     name        = "blog"
     description = "Allow http and https in. Allow everything out"
 
