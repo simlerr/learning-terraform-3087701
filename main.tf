bc1f97469a350c9a8cb7a9e3f0880671c2104a9a
simlerr <142019340+simlerr@users.noreply.github.com>
2025-01-31 12:31:07 -0600

    Load Balancer Run 21

--- main.tf
+++ main.tf
@@ -33,34 +33,35 @@ module "blog_vpc" {
     Environment = "dev"
   }
 }
-module "blog_autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
-  name = "blog"
-  min_size            = 1
-  max_size            = 2
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
-  instance_type       = var.instance_type
-  image_id            = data.aws_ami.app_ami.id
-}
-module "blog_alb" {
+
+module "alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
+
   name = "blog-alb"
+
   load_balancer_type = "application"
+
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
+  security_groups    = module.blog_sg.security_group_id
+
+
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "blog"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
     }
   ]
+
   http_tcp_listeners = [
     {
       port               = 80
@@ -68,10 +69,12 @@ module "blog_alb" {
       target_group_index = 0
     }
   ]
+
   tags = {
-    Environment = "dev"
+    Environment = "Dev"
   }
 }
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
