393ef5ac7cdac7f1549ed6723593f1f0e8d34d3c
simlerr <142019340+simlerr@users.noreply.github.com>
2025-01-31 12:14:03 -0600

    Load Balancer Run 20

--- main.tf
+++ main.tf
@@ -17,6 +17,8 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
+}
+
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
   name = "dev"
@@ -24,7 +26,6 @@ module "blog_vpc" {
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   enable_nat_gateway = true
-}
 
   tags = {
     Name = "HelloWorld"
